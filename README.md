dabdf287e1936ba4fc4a9fd5554c51a3790aba3c
lecrabe <remi.dannunzio@gmail.com>
2018-09-10 21:31:02 +0000

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+### Atelier  SEPAL, presentations & scripts 
+Le matériel de ce dépôt a été développé pour un atelier à Abidjan du 11 au 14 Septembre 2018, cellule MNV, équipe SEP-REDD.
+
+L'ensemble des données et scripts est ecrit pour un environnement SEPAL (https://sepal.io)
+
+Il contient
+
+- des elements theoriques sur l'algorithme BFAST  developpé par l'universite de  Wageningen et les concepts derriere l'analys de series temporelles denses 
+
+- des tutoriels pratiques sur l'utilisation de BFAST dans SEPAL
+
+- des tutoriels pratiques sur de modules en langage R pour traiter des données globales (ESA CCI, GFC) 
+
+- des tutoriels pratiques sur la création de planches d'imagerie pour supporter l'interpretation visuelle d'echantillons
+
+### Pre-requis
+
+Des informations generales sur OpenFOris sont disponibles ici: 
+www.openforis.org
+
+Pour obtenir acces a SEPAL, veuillez:
+
+1/ avoir un compte GMAIL
+
+2/ enregistrer le compte sur la liste blanche de Google Earth Engine https://earthengine.google.com/signup/
+
+3/ demander un compte SEPAL ici: https://tinyurl.com/sepal-access
+
---
+++ scripts/s7_segmentation.bash
@@ -0,0 +1,35 @@
+####################################################################################################
+## Segmentation of the mosaic (bash)
+## remi.dannunzio@fao.org
+## 2018/08/21
+####################################################################################################
+####################################################################################################
+
+cd ~/ws_uga_20180828/
+home=`pwd`
+workdir=$home"/data/mosaic_lsat/"
+segdir=$home"/data/segments/"
+
+#################### PERFORM SEGMENTATION USING THE OTB-SEG ALGORITHM
+p1=3   # radius of smoothing (pixels)
+p2=16  # radius of proximity (pixels)
+p3=0.1 # radiance threshold 
+p4=50  # iterations of algorithm
+p5=5   # segment minimum size (pixels)
+
+
+for input in `ls $workdir/*.tif`;
+  do echo $input;
+  
+  otbcli_MeanShiftSmoothing      -in $input         -fout $workdir/tmp_smooth.tif   -foutpos $workdir/tmp_pos.tif -spatialr $p1 -ranger $p2 -thres $p3 -maxiter $p4;
+  
+  otbcli_LSMSSegmentation        -in $workdir/tmp_smooth.tif -inpos $workdir/tmp_pos.tif     -out $workdir/tmp_seg_lsm.tif -spatialr $p1 -ranger $p2 -minsize 0 -tmpdir $workdir -tilesizex 512 -tilesizey 512;
+  
+  otbcli_LSMSSmallRegionsMerging -in $workdir/tmp_smooth.tif -inseg $workdir/tmp_seg_lsm.tif -out $segdir/seg_${input##*/} -minsize $p5 -tilesizex 512 -tilesizey 512;
+  
+  rm $workdir/tmp*.tif;
+  
+  gdal_calc.py -A $input --co COMPRESS=LZW --overwrite --outfile=$segdir/mask_${input##*/} --calc="(A>0)";
+  done;
+
+
